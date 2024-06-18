7e956284bf424293e925ec75a5e9e5d278247ff0
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2024-06-18 08:45:58 +0530

    mail option added

--- src/index.js
+++ src/index.js
@@ -13,3 +13,23 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const mailOptions = {
+  from: {
+    name:"Atul Thakre",
+    address: process.env.USER_NAME
+  },
+  to: "mybgmigaming24@gmail.com",
+  subject: "Check mail",
+  text: "Hello bgmi",
+  html: "<center><h1>Atul Thakre</h1><p>This is First mail</p></center>"
+}
+
+async function mailSned(transporter, mailOptions) {
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (e) {
+    console.log("Error " + e);
+  }
+}
+
+mailSned(transporter, mailOptions);
